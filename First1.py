86c8a8d1f9d7b9a72452730c38476958146b3492
Demonslyr <Demonslyr@gmail.com>
2015-05-21 21:34:38 -0500

    First attemp at code work

---
+++ First1.py
@@ -0,0 +1,19 @@
+def add(x, y):
+    z=x+y
+    return z
+
+def sub(x,y):
+    z=x-y
+    return z
+
+def mult(x,y):
+    z=x*y
+    return z
+
+def Hello():
+    z = "goodbye"
+    return z
+
+def math(x):
+    z=x(2,3)
+    return z
