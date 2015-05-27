43ab7548494dc2db12913b0b617ef31fc8958e46
Demonslyr <Demonslyr@gmail.com>
2015-05-26 20:18:17 -0500

    Round two
    Loops and lists

--- First1.py
+++ First1.py
@@ -16,3 +16,18 @@ def Hello():
 def math(x):
     z=x(2,3)
     return z
+
+#list_A = [0,1,2,3,4,5,6]
+list_A = [1,1,1,1,1,1,1]
+list_B = range(10)
+
+list_C = range(4,48,11)
+
+def Funk_list_add(x,y):
+    for i in range(len(x)):
+        print(x[i]+y[i])
+
+
+#while(True):
+#    print("hello")
+        
