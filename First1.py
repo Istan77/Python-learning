783aee14adbe85210e0a7e3d3726f27cd2909efd
Demonslyr <Demonslyr@gmail.com>
2015-05-28 22:38:13 -0500

    Dan M made a few updates

--- First1.py
+++ First1.py
@@ -46,25 +46,33 @@ print(x) #not casting the char
 print( int(y) ) #casting the char to an int
 '''
 
+print("Welcome to Dan's calculator!\n\n")
+
 while True:
-    x = input("type a number...")
-    y = input("type a number...")
-    c = input("select the number of the operation you would like to perform\n\t 1 = Add\n\t2 =Subtract\n\t3 = Multiply\n")
+    x = input("\n\nInput the first number...\n")
+    y = input("Input the second number...\n")
 
     x = int(x)
     y = int(y)
-    c = int(c)
-    
-    if c == 1:
-        print(add(x,y))
-    elif c == 2:
-        print(sub(x,y))
-    elif c == 3:
-        print(mult(x,y))
-    else:
-        print("ya fucked up!")
+        
+    while True:
+        c = input("select the number of the operation you would like to perform\n\t 1 = Add\n\t2 =Subtract\n\t3 = Multiply\n")
+        c = int(c)
+        
+        if c == 1:
+            print("The answer is "+str(add(x,y)))
+            break
+        elif c == 2:
+            print("The answer is "+str(sub(x,y)))
+            break
+        elif c == 3:
+            print("The answer is "+str(mult(x,y)))
+            break
+        else:
+            print("ya fucked up!")
 
     e = int(input("would you like to stop?\n\t1 = Yes\n\t2 = No"))
 
-    if e == 2:
+    if e == 1:
+        print("\nGoodbye!\n")
         break          
